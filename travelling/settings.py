fcd3bd5a80eac76cae2e50d6710f176f77cf73b5
harshita999-number <hy99933@gmail.com>
2026-02-27 14:12:44 +0530

    change

--- travelling/settings.py
+++ travelling/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("django-insecure-lc@##6-36f=k-3=)-%2fe96&o9m_(xx%dd4%(=l5ea&tpv#f*&")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True")=="True"
